32ee3585615e52211ccbcba8f59cce9a18e95934
Veenali Newalkar <veenalinewalkar1@gmail.com>
2021-07-06 13:36:14 +0530

    make home page

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const ejsMate = require('ejs-mate')
 const port = process.env.PORT || 3000
 
 
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 app.engine('ejs', ejsMate)
